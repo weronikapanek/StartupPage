8dad725352462827ca0d5e188ee07fd873662161
deving1 <werkap8811@gmail.com>
2020-05-24 09:53:50 +0200

    Startup page

--- code.js
+++ code.js
@@ -2,15 +2,10 @@ $(document).on('scroll', function () {
 
   const windowHeight = $(window).height()
   const scrollValue = $(this).scrollTop();
-  // console.log(scrollValue);
 
-
-  //Pierwsza sekcja
   const $art2 = $('.art2');
   const art2FromTop = $art2.offset().top
-  // console.log(art2FromTop);
   const art2Height = $art2.outerHeight()
-  // console.log(art2Height);
 
   const $art3 = $('.art3');
   const art3FromTop = $art3.offset().top
@@ -35,8 +30,6 @@ $(document).on('scroll', function () {
     $art4.addClass('active');
   }
 
-  // Druga sekcja
-
   const $op1 = $('.op1');
   const $op2 = $('.op2');
 
@@ -54,10 +47,6 @@ if(scrollValue > op2FromTop + op2Height / 2 - windowHeight) {
     $op2.addClass('active')
 }
 
-
-
-
-    //Czyściciel
     if (scrollValue < 100) {
       $('article').removeClass('active');
     }
